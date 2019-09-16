5ff19ccaba8c2006d4beceebb810c6adf209da23
Nahlablue28 <39217847+Nahlablue28@users.noreply.github.com>
2019-09-15 22:33:52 -0400

    Add files via upload

---
+++ Python Introduction Class Work.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print ('Hello World and Welcome to learning Python Programming from scratch.')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+list = ['apples', 'pears', 'bananas']
+print (list)
+
+
+# In[3]:
+
+
+list = ['apples', 'pears', 'bananas']
+list.append ('peaches')
+print (list)
+
+
+# In[8]:
+
+
+names =['shantel', 'sky', 'anaiyah']
+for y in names:
+    print(y)
+
+
+# In[ ]:
+
+
+names =['shantel', 'sky', 'anaiyah']
+for y in names:
+    names.append('shauna')
+    print(y)
+
+
+# In[34]:
+
+
+names =['shantel', 'sky', 'anaiyah']
+for y in names:
+
+    if y == "anaiyah":
+     break
+    print(y)
+     
+    
+   
+    
+
+
+# In[33]:
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+
+# In[2]:
+
+
+#letter the user choose when to quit:
+prompt = "\nTell me something, and I will repeat it."
+prompt +="\nEnter 'quit to end the program'"
+message = " "
+while message !='quit':
+    message = input(prompt)
+    print(message)
+
+
+# In[ ]:
+
+
+# introduction to Functions
+#f(x) = a+b
+a=1
+b=2
+f(x)=3
+
+#doc strings--give user an understanding of the code
+
+
+
+# In[10]:
+
+
+#defining a function in python:
+
+def Greet_user():
+
+"""Display a simple greeting"""
+print("Hello")
+
+Greet_user()
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
